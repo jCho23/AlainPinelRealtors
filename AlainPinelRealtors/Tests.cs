6131281d5c9855212a1c897484eabb5e9c7f5728
JuneCho23 <junecho.ucb@gmail.com>
2017-03-03 08:32:31 -0800

    Test Automation Scripts for AlainPine

---
+++ AlainPinelRealtors/Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace AlainPinelRealtors
+{
+	[TestFixture(Platform.Android)]
+	[TestFixture(Platform.iOS)]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
+
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
+
+		[Test]
+		public void Repl()
+		{
+			app.Repl();
+		}
+
+		[Test]
+		public void FirstTest()
+		{
+			//app.Tap("button1");
+			app.WaitForElement(x => x.Marked("button1"));
+			app.Tap("button1");
+
+			//app.Query(x => x.Marked("button1").Invoke("callOnClick"));
+
+			//app.Flash(x => x.Marked("menuImage").Index(0));
+			//app.Query(x => x.Marked("menuImage").Index(1).Invoke("callOnClick"));
+
+
+
+
+
+		}
+
+
+
+	}
+}
